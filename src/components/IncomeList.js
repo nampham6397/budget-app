d34695c92667c3b9e95b38f1dfe5f338181a1943
Phạm Nguyễn Minh Nam <nampnm@vmodev.com>
2021-10-20 17:35:58 +0700

    first commit

---
+++ src/components/IncomeList.js
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState'
+import IncomeTransaction from './IncomeTransactions';
+
+function IncomeList() {
+    const {incomeTransactions} = useContext(GlobalContext)
+    console.log(incomeTransactions)
+
+    return (
+        <div className='transactions transactions-income'>
+            <h2>Transaction History</h2>
+            <ul className='transaction-list'>
+                {incomeTransactions.map(incomeTransaction => (
+                <IncomeTransaction key={incomeTransaction.id} incomeTransaction={incomeTransaction} />
+            ))}   
+            </ul>
+        </div>
+    )
+}
+
+export default IncomeList
